71e88b1031385ad9e5d337607bd40cc8781a1a4d
Aaron Carson <aaron.carson783@gmail.com>
2017-12-05 19:41:36 +0000

    mess

---
+++ 1/1-2.py
@@ -0,0 +1,51 @@
+def fileReadIn(filename): #allows each files to be easily imported
+    File = open(filename,"r")
+    FileRead = File.readlines()
+    File.close()
+    Temp = []
+    Final = []
+    for x in FileRead: #formats list to be easily laid out with no \n etc
+        Temp.append(x.strip())    
+    for y in range(len(Temp)): #separates comma-separated values into individual items in lists
+        Final.append(Temp[y].split(","))        
+    return Final
+
+inFile = fileReadIn("1-1.txt")
+sumList = []
+comparison = None
+for current in range(len(inFile[0][0])):
+    circleForwards = current + (len(inFile[0][0])//2)
+##    if circleForwards > len(inFile[0][0]):
+##        circleForwards -= len(inFile[0][0])
+    try:
+        go = circleForwards
+        if inFile[0][0][circleForwards] != comparison:
+            #print(current)
+            comparison = inFile[0][0][circleForwards] 
+        else:
+            sumList.append(inFile[0][0][circleForwards])
+    except IndexError:
+        go = inFile[0][0][circleForwards-len(inFile[0][0])] + " bad"
+        if inFile[0][0][circleForwards-len(inFile[0][0])] != comparison:
+            #print(current)
+            comparison = inFile[0][0][circleForwards-len(inFile[0][0])] 
+        else:
+            sumList.append(inFile[0][0][circleForwards-len(inFile[0][0])])
+        #print(sumList)
+        print(go)
+##    try:
+##        ahh = inFile[0][0][circleForwards]
+##    except IndexError:
+##        if inFile[0][0][circleForwards] != comparison:
+##            #print(current)
+##            comparison = inFile[0][0][0] 
+##        else:
+##            sumList.append(inFile[0][0][0])
+##            #print(sumList)
+
+total = 0
+for num in sumList:
+    total += int(num)
+
+print(total)
+        
